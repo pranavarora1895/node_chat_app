8a3f746be7b5460c3746a91369181a4b2ddd0b69
Pranav Arora <48170643+pranavarora1895@users.noreply.github.com>
2021-04-01 19:09:25 +0530

    Initial Commit

---
+++ js/client.js
@@ -0,0 +1,46 @@
+const PORT = process.env.PORT || 8000
+const socket = io('http://localhost:'+PORT, { transports : ['websocket'] });
+
+const form = document.getElementById('send-container');
+const messageInput = document.getElementById('messageInp');
+const messageContainer = document.querySelector('.container');
+const nameHeading = document.querySelector('.heading-primary')
+var audio = new Audio('chat.mp3');
+
+const append = (message, position)=>{
+    const messageElement = document.createElement('div');
+    messageElement.innerText = message;
+    messageElement.classList.add('message');
+    messageElement.classList.add(position);
+    messageContainer.append(messageElement);
+    if(position=='left'){
+    audio.play();
+    }
+}
+
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    const message = messageInput.value;
+    append(`You: ${message}`, 'right');
+    socket.emit('send', message);
+    messageInput.value = '';
+})
+
+const names = prompt("Enter your name to join");
+socket.emit('new-user-joined', names);
+
+const nameHeadingElement = document.createElement('div');
+nameHeadingElement.innerText = names;
+nameHeading.append(nameHeadingElement);
+
+socket.on('user-joined', names=>{
+    append(`${names} joined the chat`, 'right')
+})
+
+socket.on('receive', data=>{
+    append(`${data.name}: ${data.message}`, 'left')
+})
+
+socket.on('left', names=>{
+    append(`${names} left the chat`, 'right')
+})
